3974d8f0a14d1dc7afcae9e20e77e148803e9973
Vivek Raman <vr.ac4bf@live.com>
2024-09-14 16:16:31 -0700

    Cleanup inputs

--- nrmp.py
+++ nrmp.py
@@ -1,4 +1,19 @@
+def cleanup_dict(dictionary):
+  cleaned = dict()
+  for key, raw_value in dictionary.items():
+    cleaned[key] = raw_value.split()
+  return cleaned
+
+
 def nrmp (the_input, quota = 1 ):
+  hospital = the_input[0]
+  student = the_input[1]
+  preference_hospitals = cleanup_dict(the_input[2])
+  preference_students = cleanup_dict(the_input[3])
+
+  print(preference_hospitals)
+  print(preference_students)
+
   # while some hospital hi has vacancy and not interviewed all on its preference list,
     # hospital hi offers position to the next student sj
     # if sj is free,
