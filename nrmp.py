c7ba64ee4cee4d3a10e02d54f87d5e77fd8a3fba
Vivek Raman <vr.ac4bf@live.com>
2024-09-14 16:14:37 -0700

    Add pseudocode

--- nrmp.py
+++ nrmp.py
@@ -1,6 +1,13 @@
-# Fill in all the missing codes in this file
-
-def nrmp (input, quota = 1 ):
-    print ('input')
-    pass
-
+def nrmp (the_input, quota = 1 ):
+  # while some hospital hi has vacancy and not interviewed all on its preference list,
+    # hospital hi offers position to the next student sj
+    # if sj is free,
+      # sj accepts
+    # if not, then sj is already committed to hk
+      # if sj prefers hk over hi then,
+        # sj remains committed to hk; do nothing
+      # else,
+        # sj commits to hi
+        # increase available slots at hk
+        # decrease available slots at hi
+  return dict()
